6bff83533eeee074c8daf2dce19083759a13262a
Kit Peters <christopher.peters@cbsinteractive.com>
2020-12-04 13:03:10 -0600

    Retrieves TLEs from hardcoded mission ID

--- assembly/index.ts
+++ assembly/index.ts
@@ -38,6 +38,8 @@ function main(req: Request): Response {
     Console.log('Invalid method "' + req.method() + '"' + '\n')
     return new Response(String.UTF8.encode('This method is not allowed'), {
       status: 405,
+      url: null,
+      headers: null,
     })
   }
   Console.log('Valid method (' + req.method() + ')' + '\n')
@@ -60,34 +62,38 @@ function main(req: Request): Response {
     const response = handleTest(urlParts[1])
     return response
   } else {
-    const payloadId = 'Thaicom 6'
+    const missionId = 'F4F83DE'
     Console.log('initializing plotter' + '\n')
     const plotter = new SpaceXPlotter(n2yoApiKey)
     Console.log('initialized plotter' + '\n')
-    const noradIds = plotter.noradIds(payloadId)
-    if (noradIds) {
-      Console.log('Got NORAD IDs\n')
-      const tles = plotter.tles(noradIds[0])
-      if (tles) {
-        Console.log('Got TLEs\n')
-        return new Response(String.UTF8.encode(tles.join('\n')), {
-          status: 200,
-        })
-      } else {
-        Console.log('No TLEs, return NORAD IDs\n')
-        return new Response(String.UTF8.encode(noradIds.toString()), {
-          status: 200,
-        })
+    const payloadTLEs = plotter.payloadTLEs(missionId)
+    if (payloadTLEs) {
+      Console.log('Got Payload TLEss\n')
+      const obj = new JSON.Obj()
+      const payloadIDs = payloadTLEs.keys()
+      for (let i = 0; i < payloadIDs.length; i++) {
+        const payloadId = payloadIDs[i]
+        const arr = new JSON.Arr()
+        const tles = payloadTLEs.get(payloadId)
+        for (let j = 0; j < tles.length; j++) {
+          arr.push(new JSON.Str(tles[j]))
+        }
+        obj.set(payloadId, arr)
       }
+      return new Response(String.UTF8.encode(obj.toString()), {
+        status: 200,
+        headers: headers([['Content-Type', 'application/json']]),
+        url: null,
+      })
     } else {
       const err = plotter.lastError()
       if (!err) {
         return new Response(
-          String.UTF8.encode(
-            'No NORAD IDs found for payload "' + payloadId + '"'
-          ),
+          String.UTF8.encode('No TLEs found for mission "' + missionId + '"'),
           {
             status: 404,
+            headers: null,
+            url: null,
           }
         )
       } else {
@@ -223,9 +229,29 @@ function testRequestSpaceX(): string {
   return response_text
 }
 
+type HeaderArray = Array<string[]>
+
+function headers(h: HeaderArray): Headers {
+  const hdrs = new Headers()
+  for (let i = 0; i < h.length; i++) {
+    hdrs.set(h[i][0], h[i][1])
+  }
+  return hdrs
+}
+
+type PayloadTLEs = Map<string, string[]>
+
+class RequestResult {
+  json: JSON.Obj | null
+  text: string | null
+  status: u16
+  statusText: string
+}
+
 class SpaceXPlotter {
   readonly n2yoApiKey: string
   private _lastError: _Error | null
+
   constructor(n2yoApiKey: string) {
     this.n2yoApiKey = n2yoApiKey
   }
@@ -237,6 +263,128 @@ class SpaceXPlotter {
    */
   public lastError(): _Error | null {
     return this._lastError
+  }
+
+  /**
+   * Get the TLEs for each payload in the given mission
+   *
+   * @param missionId
+   * @returns Map of payload ID to TLE array
+   */
+  public payloadTLEs(missionId: string): PayloadTLEs | null {
+    Console.log('Request paylod TLEs for mission "' + missionId + '"\n')
+    this._lastError = null
+    const payloadIds = this.payloadIds(missionId)
+    if (payloadIds) {
+      const payloadTLEs = new Map<string, string[]>()
+      for (let i = 0; i < payloadIds.length; i++) {
+        const payloadId = payloadIds[i]
+        const noradIds = this.noradIds(payloadId)
+        if (noradIds) {
+          const tles = this.getTLEsForNoradIds(noradIds)
+          if (!tles) {
+            return null
+          }
+          payloadTLEs.set(payloadId, tles)
+        } else {
+          if (this.lastError()) {
+            return null
+          } else {
+            Console.log(
+              'ERROR: No NORAD IDs for payload ' +
+                payloadId +
+                ' (mission ' +
+                missionId +
+                ')\n'
+            )
+            this._lastError = new _Error(
+              'No NORAD IDs for payload ' +
+                payloadId +
+                ' (mission ' +
+                missionId +
+                ')'
+            )
+            return null
+          }
+        }
+      }
+      return payloadTLEs
+    } else {
+      Console.log('No payload IDs found for mission ' + missionId)
+      return null
+    }
+  }
+
+  private getTLEsForNoradIds(noradIds: i64[]): string[] | null {
+    const tles: string[] = []
+    for (let j = 0; j < noradIds.length; j++) {
+      const noradId = noradIds[j]
+      const _tles = this.tles(noradId)
+      if (_tles) {
+        for (let k = 0; k < _tles.length; k++) {
+          tles.push(_tles[k])
+        }
+      } else {
+        if (this.lastError()) {
+          return null
+        } else {
+          Console.log(
+            'ERROR: No TLEs for NORAD ID ' + noradId.toString() + '\n'
+          )
+          this._lastError = new _Error(
+            'No TLEs found for NORAD ID ' + noradId.toString()
+          )
+        }
+      }
+    }
+    return tles
+  }
+
+  public payloadIds(missionId: string): string[] | null {
+    Console.log('Request paylod IDs for mission "' + missionId + '"\n')
+    this._lastError = null
+    const res = this.spacexRequest(
+      '{ mission(id: "' + missionId + '") { payloads { id } } }'
+    )
+    if (!res) {
+      const err = this.lastError()
+      if (err) {
+        Console.log('ERROR: ' + err.toString() + '\n')
+      } else {
+        Console.log('No payload IDs found\n')
+      }
+      return null
+    }
+    const data = res.get('data')
+    if (!data) {
+      Console.log('ERROR: SpaceX response missing "data" field\n')
+      this._lastError = new _Error('"data" field missing in SpaceX response')
+      return null
+    }
+    const mission = <JSON.Obj>(<JSON.Obj>data).get('mission')
+    if (mission) {
+      if (mission.has('payloads')) {
+        const payloads = <JSON.Arr>mission.get('payloads')
+        debug('payloads internal array: ' + payloads._arr.toString())
+        const payloadIds: string[] = payloads._arr.map<string>(
+          (val: JSON.Value) => {
+            const obj = <JSON.Obj>val
+            const payloadId = (<JSON.Str>obj.get('id'))._str
+            return payloadId
+          }
+        )
+        Console.log(
+          payloadIds.length.toString() +
+            ' payload IDs found: ' +
+            payloadIds.toString() +
+            '\n'
+        )
+        return payloadIds
+      }
+      Console.log('No payload IDs found\n')
+    }
+    Console.log('WARNING: No "mission" in SpaceX response')
+    return null
   }
 
   /**
@@ -273,9 +421,14 @@ class SpaceXPlotter {
         const noradIds: i64[] = norad_ids._arr.map<i64>(
           (val: JSON.Value) => (<JSON.Num>val)._num
         )
-        Console.log(noradIds.length.toString() + ' NORAD IDs found: ' + noradIds.toString() + '\n')
+        Console.log(
+          noradIds.length.toString() +
+            ' NORAD IDs found: ' +
+            noradIds.toString() +
+            '\n'
+        )
         return noradIds
-      } 
+      }
       Console.log('No NORAD IDs found\n')
     }
     Console.log('WARNING: No "payload" in SpaceX response')
@@ -320,22 +473,25 @@ class SpaceXPlotter {
     Console.log('Request path "' + path + '" from N2YO API\n')
     const uri = n2yoUri + path + '?apiKey=' + n2yoApiKey
 
-    const response = this.request('GET', uri, BACKEND_N2YO)
-    const status = response.status()
-    const statusText = response.statusText()
-    const response_text = response.text()
+    const result = this.request('GET', uri, BACKEND_N2YO)
+    const status = result.status
+    const statusText = result.statusText
+    const responseText: string = <string>(result.text || '')
+    debug('N2YO response text: ' + responseText)
     if (status != 200) {
       let message =
-        'Remote responded with ' + status.toString() + ' ' + statusText
-      if (response_text) {
-        message += '\nResponse text:\n' + response_text + '\n'
-      }
+        'Remote ' +
+        BACKEND_N2YO +
+        ' responded with ' +
+        status.toString() +
+        ' ' +
+        statusText
       this._lastError = new _Error(message)
       return null
     }
-    Console.log('Response text: ' + response_text + '\n')
-    return <JSON.Obj>JSON.parse(response_text)
+    return result.json
   }
+
   /**
    * Send a request to the SpaceX GraphQL API
    *
@@ -351,26 +507,34 @@ class SpaceXPlotter {
     const queryObj = new JSON.Obj()
     queryObj.set('query', query)
 
-    const response = this.request(
+    const result = this.request(
       'POST',
       spaceXUri,
       BACKEND_SPACEX,
       headers,
       queryObj.toString()
     )
-    const status = response.status()
-    const statusText = response.statusText()
-    const response_text = response.text()
+    const status = result.status
+    const statusText = result.statusText
+    const responseText: string = <string>(result.text || '')
+    debug('SpaceX response text: ' + responseText)
     if (status != 200) {
       let message =
-        'Remote responded with ' + status.toString() + ' ' + statusText
-      if (response_text) {
-        message += '\nResponse text:\n' + response_text + '\n'
-      }
+        'Remote ' +
+        BACKEND_SPACEX +
+        ' responded with ' +
+        status.toString() +
+        ' ' +
+        statusText
       this._lastError = new _Error(message)
       return null
     }
-    return <JSON.Obj>JSON.parse(response_text)
+    if (!result.json) {
+      this._lastError = new _Error(
+        'Remote did not respond with JSON. Response text: ' + responseText
+      )
+    }
+    return result.json
   }
 
   private request(
@@ -379,7 +543,7 @@ class SpaceXPlotter {
     backend: string,
     headers: Headers = new Headers(),
     body: string = ''
-  ): Response {
+  ): RequestResult {
     const init: RequestInit = {
       method: method,
       headers: headers || new Headers(),
@@ -394,15 +558,26 @@ class SpaceXPlotter {
       backend: backend,
       cacheOverride: null,
     }).wait()
+    const response_text = response.text()
     const response_headers = response.headers()
     const keys = response_headers.keys()
-    let key: string
     for (let i = 0; i < keys.length; i++) {
       let key: string = keys[i]
       const val: string = <string>(response_headers.get(key) || '')
       Console.log('Response header "' + key + '": "' + val + '"\n')
     }
-    return response
+
+    const result: RequestResult = {
+      json: null,
+      text: response_text,
+      status: response.status(),
+      statusText: response.statusText(),
+    }
+    let contentType = response_headers.get('content-type')
+    if (contentType && contentType.includes('application/json')) {
+      result.json = <JSON.Obj>JSON.parse(response_text)
+    }
+    return result
   }
 }
 
@@ -422,4 +597,24 @@ class _Error {
     str += this.message
     return str
   }
+}
+
+function debug(message: string): void {
+  log(message, 'DEBUG')
+}
+function info(message: string): void {
+  log(message, 'INFO')
+}
+function warn(message: string): void {
+  log(message, 'WARN')
+}
+function error(message: string): void {
+  log(message, 'ERROR')
+}
+function fatal(message: string): void {
+  log(message, 'FATAL')
+}
+
+function log(message: string, level: string = 'INFO'): void {
+  Console.log('[' + level + '] ' + message + '\n')
 }
