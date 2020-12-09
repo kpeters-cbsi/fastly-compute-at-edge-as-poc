0269c17b2fe20145e86e03082bba531d34f4379c
Kit Peters <christopher.peters@cbsinteractive.com>
2020-12-09 15:17:34 -0600

    Switch from GraphQL to REST for SpaceX
    Fastly won't cache POST requests, and the SpaceX GraphQL API doesn't
    support GET requests.

--- assembly/index.ts
+++ assembly/index.ts
@@ -6,20 +6,20 @@ import {
   RequestInit,
 } from '@fastly/as-compute'
 import { JSON } from './lib/assemblyscript-json'
-import { Console } from 'as-wasi'
+import { Console, Date } from 'as-wasi'
 
 /**
- * Maximum number of payloads to return for a given misison
+ * Maximum number of HTTP transactions to execute
  *
  * Necessary because Fastly imposes a limit of 8 requests / execution
  */
-const PAYLOAD_LIMIT = 3
+const TXN_LIMIT = 6
 
-const BACKEND_SPACEX = 'SpaceX'
+const BACKEND_SPACEXDATA = 'SpaceXData'
 const BACKEND_N2YO = 'n2yo'
 
 const n2yoApiKey = 'YBLNQJ-JUG3KB-XS5BRT-1JX2'
-const spaceXUri = 'https://api.spacex.land/graphql/'
+const spaceXDataUri = 'https://api.spacexdata.com/v3/'
 const n2yoUri = 'https://api.n2yo.com/rest/v1/satellite/'
 
 // The entry point for your application.
@@ -29,9 +29,8 @@ const n2yoUri = 'https://api.n2yo.com/rest/v1/satellite/'
 // the request to a backend, make completely new requests, and/or generate
 // synthetic responses.
 function main(req: Request): Response {
+  debug('******************************************************')
   debug('Request: ' + req.method() + ' ' + req.url())
-
-  Console.error('This is a test error\n')
 
   // We can filter requests that have unexpected methods.
   const VALID_METHODS = ['GET']
@@ -47,10 +46,6 @@ function main(req: Request): Response {
   debug('Host: ' + host)
   debug('Path: ' + path)
 
-  let limit: i32 = PAYLOAD_LIMIT
-  if (req.headers().has('x-payload-limit')) {
-    limit = <i32>Number.parseInt(<string>req.headers().get('x-payload-limit'))
-  }
   if (urlParts[0] == 'tle') {
     if (urlParts.length < 2) {
       urlParts.push('')
@@ -61,7 +56,7 @@ function main(req: Request): Response {
       debug('Initializing plotter')
       const plotter = new SpaceXPlotter(n2yoApiKey)
       debug('Initialized plotter')
-      const payloadTLEs = plotter.payloadTLEs(missionId, limit)
+      const payloadTLEs = plotter.payloadTLEs(missionId)
       const responseObj = new JSON.Obj()
       const missionObj = new JSON.Obj()
       const payloadsArr = new JSON.Arr()
@@ -83,9 +78,12 @@ function main(req: Request): Response {
         if (err) {
           return internalServerError(err.toString())
         }
+        return notFound('No TLEs found for mission ' + missionId)
       }
       responseObj.set('payloads', payloadsArr)
-      return ok(responseObj.toString())
+      const headers = new Headers()
+      headers.set('content-type', 'application/json')
+      return ok(responseObj.toString(), headers)
     }
   }
 
@@ -136,13 +134,6 @@ Fastly.respondWith(resp)
 
 type HeaderArray = Array<string[]>
 
-function headers(h: HeaderArray): Headers {
-  const hdrs = new Headers()
-  for (let i = 0; i < h.length; i++) {
-    hdrs.set(h[i][0], h[i][1])
-  }
-  return hdrs
-}
 
 type PayloadTLEs = Map<string, string[]>
 
@@ -156,11 +147,15 @@ class RequestResult {
 class SpaceXPlotter {
   readonly n2yoApiKey: string
   private _lastError: _Error | null
+  private _txnCount: i32
 
   constructor(n2yoApiKey: string) {
     this.n2yoApiKey = n2yoApiKey
   }
 
+  public txnCount(): i32 {
+    return this._txnCount
+  }
   /**
    * Return the most recent error, if any, encountered in the last method call.
    *
@@ -176,63 +171,71 @@ class SpaceXPlotter {
    * @param missionId
    * @returns Map of payload ID to TLE array
    */
-  public payloadTLEs(
-    missionId: string,
-    payloadLimit: i32 = 0
-  ): PayloadTLEs | null {
-    Console.log('Request paylod TLEs for mission "' + missionId + '"\n')
+  public payloadTLEs(missionId: string): PayloadTLEs | null {
+    let t0: f64 = Date.now()
+    debug('Request payload TLEs for mission "' + missionId)
     this._lastError = null
-    const payloadIds = this.payloadIds(missionId, payloadLimit)
-    if (payloadIds) {
+    const noradIds = this.noradIdsForMission(missionId)
+    this.logElapsed(t0, 'get NORAD IDs')
+    if (noradIds) {
       const payloadTLEs = new Map<string, string[]>()
-      if (payloadLimit) {
-        info('Limit TLEs to ' + payloadLimit.toString() + ' payloads')
-      } else {
-        payloadLimit = payloadIds.length
-      }
+      const payloadIds = noradIds.keys()
       for (let i = 0; i < payloadIds.length; i++) {
         const payloadId = payloadIds[i]
-        if (i < payloadLimit) {
-          const noradIds = this.noradIds(payloadId)
-          if (noradIds) {
-            const tles = this.getTLEsForNoradIds(noradIds)
-            if (!tles) {
-              return null
-            }
-            payloadTLEs.set(payloadId, tles)
+        let t0 = Date.now()
+        let noradIdsForPayload = noradIds.get(payloadId)
+        const txnCount = this.txnCount()
+        debug(
+          'TXN count: ' +
+            txnCount.toString() +
+            ' TXN limit: ' +
+            TXN_LIMIT.toString()
+        )
+        if (txnCount < TXN_LIMIT) {
+          const limit = TXN_LIMIT - txnCount
+          if (limit > noradIdsForPayload.length) {
+            debug(
+              'Get TLEs for all ' +
+                noradIdsForPayload.length.toString() +
+                ' NORAD IDs in payload'
+            )
           } else {
-            if (this.lastError()) {
-              return null
-            } else {
-              Console.log(
-                'ERROR: No NORAD IDs for payload ' +
-                  payloadId +
-                  ' (mission ' +
-                  missionId +
-                  ')\n'
-              )
-              this._lastError = new _Error(
-                'No NORAD IDs for payload ' +
-                  payloadId +
-                  ' (mission ' +
-                  missionId +
-                  ')'
-              )
-              return null
-            }
+            debug(
+              'Get ' +
+                limit.toString() +
+                '/' +
+                noradIdsForPayload.length.toString() +
+                ' TLEs'
+            )
           }
+          // only get as many TLEs as we have available requests
+          noradIdsForPayload = noradIdsForPayload.slice(0, limit)
+          debug('Get TLEs for NORAD IDs ' + noradIdsForPayload.toString())
+          const tles = this.getTLEsForNoradIds(noradIdsForPayload)
+          this.logElapsed(
+            t0,
+            'get TLEs for NORAD IDs ' + noradIdsForPayload.toString()
+          )
+          if (!tles) {
+            return null
+          }
+          payloadTLEs.set(payloadId, tles)
         } else {
           payloadTLEs.set(payloadId, [])
         }
       }
+      this.logElapsed(t0, 'payloadTLEs()')
       return payloadTLEs
     } else {
-      Console.log('No payload IDs found for mission ' + missionId)
+      if (!this.lastError()) {
+        info('No NORAD IDs found for mission ' + missionId)
+      }
       return null
     }
   }
 
   private getTLEsForNoradIds(noradIds: i64[]): string[] | null {
+    debug('Get TLEs for NORAD IDs ' + noradIds.toString())
     const tles: string[] = []
     for (let j = 0; j < noradIds.length; j++) {
       const noradId = noradIds[j]
@@ -257,100 +260,52 @@ class SpaceXPlotter {
     return tles
   }
 
-  public payloadIds(missionId: string, limit: i32): string[] | null {
-    debug('Request paylod IDs for mission "' + missionId)
+  private noradIdsForMission(missionId: string): Map<string, i64[]> | null {
+    debug('Request NORAD IDs for mission "' + missionId + '"')
     this._lastError = null
-    const res = this.spacexRequest(
-      '{ mission(id: "' + missionId + '") { payloads { id } } }'
-    )
+    const filter = new Map<string, string>()
+    filter.set('mission_id', missionId)
+    filter.set('filter', 'rocket/second_stage/payloads/(payload_id,norad_id)')
+    const res = this.spacexDataRequest('launches', filter)
     if (!res) {
       const err = this.lastError()
       if (err) {
         error(err.toString())
       } else {
-        info('No payload IDs found')
+        info('No NORAD IDs found for mission ' + missionId)
       }
       return null
     }
-    const data = res.get('data')
-    if (!data) {
-      fatal('SpaceX response missing "data" field')
-      this._lastError = new _Error('"data" field missing in SpaceX response')
-      return null
-    }
-    const mission = <JSON.Obj>(<JSON.Obj>data).get('mission')
-    if (mission) {
-      if (mission.has('payloads')) {
-        const payloads = <JSON.Arr>mission.get('payloads')
-        debug('payloads internal array: ' + payloads._arr.toString())
-        if (payloads._arr.includes(new JSON.Null())) {
-          debug('Mission contains null payloads')
-        }
-        let payloadIds: string[] = payloads._arr
-          .map<string>((val: JSON.Value) => {
-            const obj = <JSON.Obj>val
-            const payloadId = (<JSON.Str>obj.get('id'))._str
-            return payloadId
-          })
-          .filter((payloadId) => !!payloadId)
-        debug(
-          payloadIds.length.toString() +
-            ' non-null payload IDs found: ' +
-            payloadIds.toString()
-        )
-        return payloadIds
-      }
-      info('No payload IDs found')
-    }
-    warn('No "mission" in SpaceX response')
-    return null
-  }
+    debug('NORAD IDs request successful')
+    const data = <JSON.Arr>res
+    const ret = new Map<string, i64[]>()
+    debug(data._arr.length.toString() + ' items in response')
+    for (let i = 0; i < data._arr.length; i++) {
+      const obj = <JSON.Obj>data._arr[i]
+      debug('Got obj ' + i.toString())
+      const rocket = <JSON.Obj>obj.get('rocket')
+      debug('Got obj ' + i.toString() + ' rocket')
+      const second_stage = <JSON.Obj>rocket.get('second_stage')
+      debug('Got obj ' + i.toString() + ' rocket / second_stage')
+      const payloads = <JSON.Arr>second_stage.get('payloads')
+      debug('Got obj ' + i.toString() + ' rocket / second_stage / payloads')
+      debug(payloads._arr.length.toString() + ' payloads')
+      for (let j = 0; j < payloads._arr.length; j++) {
+        const payload = <JSON.Obj>payloads._arr[j]
+        debug('got payload ' + j.toString())
+        const payloadId = (<JSON.Str>payload.get('payload_id'))._str
+        debug('got payload ' + j.toString() + ' payload_id')
 
-  /**
-   * Return the NORAD ID(s) for the given SpaceX payload
-   *
-   * @param payloadId SpaceX payload ID
-   * @returns Array of NORAD IDs. Null can indicate both 'no IDs found' and an error; check @see lastError() to see what's what
-   */
-  public noradIds(payloadId: string): i64[] | null {
-    debug('Request NORAD IDs for payload "' + payloadId)
-    this._lastError = null
-    const res = this.spacexRequest(
-      '{ payload(id: "' + payloadId + '") { norad_id } }'
-    )
-    if (!res) {
-      const err = this.lastError()
-      if (err) {
-        error(err.toString())
-      } else {
-        info('No NORAD IDs found')
-      }
-      return null
-    }
-    const data = res.get('data')
-    if (!data) {
-      fatal('ERROR: SpaceX response missing "data" field\n')
-      this._lastError = new _Error('"data" field missing in SpaceX response')
-      return null
-    }
-    const payload = <JSON.Obj>(<JSON.Obj>data).get('payload')
-    if (payload) {
-      if (payload.has('norad_id')) {
-        const norad_ids = <JSON.Arr>payload.get('norad_id')
-        const noradIds: i64[] = norad_ids._arr.map<i64>(
+        const norad_id = (<JSON.Arr>payload.get('norad_id'))._arr
+        debug('got payload ' + j.toString() + ' norad_id')
+        debug(norad_id.length.toString() + ' NORAD IDs')
+        const noradIds: i64[] = norad_id.map<i64>(
           (val: JSON.Value) => (<JSON.Num>val)._num
         )
-        debug(
-          noradIds.length.toString() +
-            ' NORAD IDs found: ' +
-            noradIds.toString()
-        )
-        return noradIds
+        ret.set(payloadId, noradIds)
       }
-      info('No NORAD IDs found')
     }
-    warn('WARNING: No "payload" in SpaceX response')
-    return null
+    return ret
   }
 
   /**
@@ -359,7 +314,7 @@ class SpaceXPlotter {
    * @param noradId
    * @returns the TLEs as an array of strings.
    */
-  public tles(noradId: i64): string[] | null {
+  private tles(noradId: i64): string[] | null {
     debug('Request TLEs for NORAD ID ' + noradId.toString())
     const path = 'tle/' + noradId.toString()
     const res = this.n2yoRequest(path)
@@ -386,9 +341,10 @@ class SpaceXPlotter {
     debug('Request path "' + path + '" from N2YO API')
     const uri = n2yoUri + path + '?apiKey=' + n2yoApiKey
 
-    const override = new Fastly.CacheOverride()
-    override.setPass()
-    const result = this.request('GET', uri, BACKEND_N2YO, null, null, override)
+    //const override = new Fastly.CacheOverride()
+    //override.setPass()
+    //const result = this.request('GET', uri, BACKEND_N2YO, null, null, override)
+    const result = this.request('GET', uri, BACKEND_N2YO, null, null)
     const status = result.status
     if (status != 200) {
       const statusText = result.statusText
@@ -405,34 +361,32 @@ class SpaceXPlotter {
     return result.json
   }
 
-  /**
-   * Send a request to the SpaceX GraphQL API
-   *
-   * Returns null and sets @see lastError() on a non-200 response
-   *
-   * @param query GraphQL query string
-   * @returns Instance of @see JSON Obj corresponding to the server response
-   */
-  private spacexRequest(query: string): JSON.Obj | null {
-    debug('Request query "' + query + '" from SpaceX API')
-    const headers = new Headers()
-    headers.set('Content-Type', 'application/json')
-    const queryObj = new JSON.Obj()
-    queryObj.set('query', query)
-
-    const result = this.request(
-      'POST',
-      spaceXUri,
-      BACKEND_SPACEX,
-      headers,
-      queryObj.toString()
-    )
+  private spacexDataRequest(
+    path: string,
+    filter: Map<string, string> = new Map<string, string>()
+  ): JSON.Value | null {
+    debug('Request path "' + path + '" from SpaceXData API')
+    let uri = spaceXDataUri + path
+    const keys = filter.keys()
+    if (keys.length) {
+      for (let i = 0; i < keys.length; i++) {
+        let joint = i == 0 ? '?' : '&'
+        const key = keys[i]
+        const val = filter.get(key)
+        uri = uri + joint + key + '=' + val
+      }
+    }
+    const override = new Fastly.CacheOverride()
+    override.setTTL(86400)
+    override.setSWR(86400)
+    override.deletePass()
+    const result = this.request('GET', uri, BACKEND_SPACEXDATA, null, null, override)
     const status = result.status
     if (status != 200) {
       const statusText = result.statusText
       let message =
         'Remote ' +
-        BACKEND_SPACEX +
+        BACKEND_SPACEXDATA +
         ' responded with ' +
         status.toString() +
         ' ' +
@@ -445,6 +399,20 @@ class SpaceXPlotter {
       this._lastError = new _Error(
         'Remote did not respond with JSON. Response text: ' + resultText
       )
+      return null
+    } else {
+      const resultJson = <JSON.Obj>result.json
+      if (resultJson.has('errors')) {
+        const errors = <JSON.Arr>resultJson.get('errors')
+        const error = <JSON.Obj>errors._arr[0]
+        if (error.has('message')) {
+          this._lastError = new _Error(
+            'Remote indicated a problem: ' +
+              (<JSON.Str>error.get('message'))._str
+          )
+          return null
+        }
+      }
     }
     return result.json
   }
@@ -457,6 +425,7 @@ class SpaceXPlotter {
     body: string | null = null,
     cacheOverride: Fastly.CacheOverride | null = null
   ): RequestResult {
+    const t0: f64 = Date.now()
     const init: RequestInit = {
       method: method,
       headers: headers,
@@ -465,17 +434,47 @@ class SpaceXPlotter {
     if (body) {
       init.body = String.UTF8.encode(body)
     }
-    debug('(' + backend + ') ' + method + ' ' + uri)
+    this._txnCount++
+    debug(
+      'TXN ' +
+        this.txnCount().toString() +
+        ' (' +
+        backend +
+        ') ' +
+        method +
+        ' ' +
+        uri
+    )
     const request = new Request(uri, init)
-    debug('Fetching request')
+    debug('TXN ' + this.txnCount().toString() + ' Fetching request')
     const response = Fastly.fetch(request, {
       backend: backend,
       cacheOverride,
     }).wait()
-    debug('(' + backend + ') Request successful')
+    debug(
+      'TXN ' +
+        this.txnCount().toString() +
+        ' (' +
+        backend +
+        ') Request complete'
+    )
     const response_text = response.text()
-    debug('(' + backend + ') Response text: ' + response_text)
+    debug(
+      'TXN ' +
+        this.txnCount().toString() +
+        ' (' +
+        backend +
+        ') Response text: ' +
+        response_text
+    )
     const response_headers = response.headers()
+    const cache = response_headers.get('x-cache')
+    const cacheHits = response_headers.get('x-cache-hits')
+    let logMessage = '(' + backend + ') Cache: ' + <string>cache
+    if (cache != 'MISS') {
+      logMessage = logMessage + ' hits: ' + <string>cacheHits
+    }
+    info('TXN ' + this.txnCount().toString() + ' ' + logMessage)
     // const keys = response_headers.keys()
     // for (let i = 0; i < keys.length; i++) {
     //   let key: string = keys[i]
@@ -491,16 +490,29 @@ class SpaceXPlotter {
     }
     let contentType = response_headers.get('content-type')
     if (contentType && contentType.includes('application/json')) {
+      debug('TXN ' + this.txnCount().toString() + ' parsing JSON')
       result.json = <JSON.Obj>JSON.parse(response_text)
+      debug('TXN ' + this.txnCount().toString() + ' parsed JSON')
     }
+    this.logElapsed(
+      t0,
+      'TXN ' + this.txnCount().toString() + ' ' + backend + ' request'
+    )
     return result
+  }
+
+  private logElapsed(t0: f64, message: string): void {
+    const t1 = Date.now()
+
+    const elapsed: f64 = t1 - t0
+    info('Elapsed in ' + message + ': ' + elapsed.toString() + ' ms')
   }
 }
 
 class _Error {
   readonly message: string
-  readonly code: string
-  constructor(message: string, code: string = '') {
+  readonly code: string | null
+  constructor(message: string, code: string | null = null) {
     this.message = message
     this.code = code
   }
@@ -520,9 +532,6 @@ function debug(message: string): void {
 }
 function info(message: string): void {
   log(message, 'INFO')
-}
-function warn(message: string): void {
-  log(message, 'WARN')
 }
 function error(message: string): void {
   log(message, 'ERROR')
